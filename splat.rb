3344e636a2a0698ce90aca0263381b0b7eb5d2b1
Nayandeep97 <nayandeepsen25111997@gmail.com>
2019-12-02 17:55:47 +0530

    Splat changed

--- splat.rb
+++ splat.rb
@@ -1,15 +1,17 @@
 def roster **player_with_position
     player_with_position.each do |player,position|
-        puts "player: #[player]"
-        puts "position: #[position]"
+        puts "player: #{player}"
+        puts "position: #{position}"
         puts"\n"
     end
 end
 
-data = {
-    "Atul":"1st pos"
-    "Vinay":"2nd po"
-    "ram":"3rd po"
+data ={
+    
+    "Vinay":"3rd base",
+    "Evan":"Catcher",
+    "Ram":"OF"
 
 }
+
 roster data
